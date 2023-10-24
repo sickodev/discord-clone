4892d1b5226f116188dceac21594cc1e7125e034
sickodev <kbishwa03kalyan@gmail.com>
2023-10-25 02:21:03 +0530

    add upload functionality

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import InitialModal from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -19,7 +20,11 @@ const SetupPage = async () => {
     if (server) {
         return redirect(`/server/${server.id}`);
     }
-    return <div>Create a Server</div>;
+    return (
+        <div>
+            <InitialModal />
+        </div>
+    );
 };
 
 export default SetupPage;
